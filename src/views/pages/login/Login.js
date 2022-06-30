5283f0a4b51ac4410a6fc47d2b809680630e3341
Anibal89 <72290798+Anibal89@users.noreply.github.com>
2022-06-30 12:11:14 -0600

    commit
    carga de data

---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import UnaImg from 'src/assets/images/unisa.png'
+import { Link } from 'react-router-dom'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+
+const Login = () => {
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <img className="d-block w-100" src={UnaImg}/>
+                    <p className="text-medium-emphasis">Iniciar sesión en su cuenta</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilUser} />
+                      </CInputGroupText>
+                      <CFormInput placeholder="Nombre de usuario" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Contraseña"
+                        autoComplete="current-password"
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton color="primary" className="px-4">
+                          Iniciar sesion
+                        </CButton>
+                      </CCol>
+                      <CCol xs={6} className="text-right">
+                        <CButton color="link" className="px-0">
+                          Forgot password?
+                        </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-dark py-5" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>
+                      Reprehenderit fugiat aute quis pariatur officia tempor duis aliquip veniam nulla nostrud. Sit deserunt eiusmod excepteur commodo elit qui quis aliquip Lorem officia tempor eiusmod consectetur. Nisi voluptate do esse Lorem minim. Laboris nisi eiusmod sint ad sint amet duis. Eiusmod voluptate adipisicing enim fugiat cupidatat. Aliqua anim aliqua commodo amet veniam sint minim velit deserunt pariatur. Excepteur excepteur sit in exercitation ad tempor amet culpa tempor qui cillum ullamco.
+                    </p>
+                    <Link to="/register">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                        Register Now!
+                      </CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
